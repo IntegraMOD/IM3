82dc8aabbc74a2bdc777e3c18baf693785f012d5
IntegraMOD <john@jwinstallations.com>
2024-08-05 08:22:33 -0700

    remove geshi

--- IntegraMOD3/assets/js/css-menu.js
+++ IntegraMOD3/assets/js/css-menu.js
@@ -1,11 +1,11 @@
 function updatemenu() {
-  if (document.getElementsByClassName('responsive-menu').checked == true) {
-    document.getElementsByClassName('menu').style.borderBottomRightRadius = '0';
-    document.getElementsByClassName('menu').style.borderBottomLeftRadius = '0';
-  }else{
-    document.getElementsByClassName('menu').style.borderRadius = '0px';
+  var menuItems = document.getElementsByClassName('menu');
+  if (document.getElementsByClassName('responsive-menu')[0].checked == true) {
+    menuItems[0].style.borderBottomRightRadius = '0';
+    menuItems[0].style.borderBottomLeftRadius = '0';
+  } else {
+    menuItems[0].style.borderRadius = '0px';
   }
-
 }
 
 
