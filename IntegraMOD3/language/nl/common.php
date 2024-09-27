43fdada4af43e407c0ccc4e457fe830c9fcb4848
IntegraMOD <john@jwinstallations.com>
2024-09-26 17:50:33 -0700

    update lang files

--- IntegraMOD3/language/nl/common.php
+++ IntegraMOD3/language/nl/common.php
@@ -970,5 +970,14 @@ $lang = array_merge($lang, array(
     'AL_LIKE_INFO'					    => 'Likes your&nbsp;',				// userX `likes your` post
 	'AL_LIKE_SELF'				        => 'View your post',
 	'AL_NO_LIKE_RECEIVED'				=> 'No like received yet.',
+	'AL_TITLE'				=> 'Last Likes',
+	'AL_NO_LIKE'			=> 'No post liked yet.',
+	'AL_BY'					=> 'posted by',			// like by
+	'AL_AT'					=> 'Liked at',			// like date
+	'AL_VIEW'				=> '[View Post]',
+	'AL_LIKE_COUNT_TEXT'	=> 'Likes',				// number of likes
+	'AL_LIKED_COUNT_TEXT'	=> 'Liked in',			// number of recieved likes
+	'AL_POSTS_TEXT'			=> 'posts',
+	'AL_POST_TEXT'			=> 'post',
 	// ajaxlike
 ));
