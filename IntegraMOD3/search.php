177ceb150f66f212d0011989d0bb8c1b015d503d
IntegraMOD <john@jwinstallations.com>
2026-04-15 16:33:04 -0700

    Update search.php
    socialnet add

--- IntegraMOD3/search.php
+++ IntegraMOD3/search.php
@@ -635,7 +635,8 @@ if ($keywords || $author || $author_id || $search_id || $submit)
 			$zebra = array();
 			while ($row = $db->sql_fetchrow($result))
 			{
-				$zebra[($row['friend']) ? 'friend' : 'foe'][] = $row['zebra_id'];
+//				$zebra[($row['friend']) ? 'friend' : 'foe'][] = $row['zebra_id'];
+                $zebra[($row['foe']) ? 'foe' : 'friend'][] = $row['zebra_id'];
 			}
 			$db->sql_freeresult($result);
 
