95620dfa5ffd84e7b83128e7027e2e93a9eeb2d1
IntegraMOD <john@jwinstallations.com>
2026-04-21 19:57:49 -0700

    Delete IntegraMOD3/styles/assets directory

--- IntegraMOD3/styles/assets/js/css-footer-menu.js
+++
@@ -1,8 +0,0 @@
-function updatefootermenu() {
-  if (document.getElementById('responsive-footer-menu').checked == true) {
-    document.getElementById('menu_footer').style.borderBottomRightRadius = '0';
-    document.getElementById('menu_footer').style.borderBottomLeftRadius = '0';
-  }else{
-    document.getElementById('menu_footer').style.borderRadius = '0px';
-  } 
-}
