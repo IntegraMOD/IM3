76b14d3b9a03b8a0f109b56697ea62db93ab9d71
IntegraMOD <john@jwinstallations.com>
2026-05-06 17:47:53 -0700

    Update kiss_common.php

--- IntegraMOD3/language/nl/portal/kiss_common.php
+++ IntegraMOD3/language/nl/portal/kiss_common.php
@@ -370,6 +370,7 @@ $lang = array_merge($lang, array(
 	'TOOLS_ON'					=> 'Portal Tools',
 	'TOOLS_OFF'					=> 'Save Changes',
 	'TOPICS_PER_FORUM_DISPLAY'	=> ' topics per forum &bull; Display: ',
+	'TOTAL_LANGUAGES'			=> 'Total available languages',
 	'TOTAL_STYLES'				=> 'Total available styles',
 
 	'UNDER_CONSTRUCTION'	=> "<strong>The page you requested is currently under construction...</strong><br /><br />Please use the 'Back' button to return to previous page.",
